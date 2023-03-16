944a38092a2ce2844a759c5e5a4ff74988e597aa
git <eae966@gmail.com>
2023-03-16 18:14:54 +0330

    Some Bug fixes.

--- apps/web/src/config/constants/supportChains.ts
+++ apps/web/src/config/constants/supportChains.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@plgswap/sdk'
 
-export const SUPPORT_ONLY_BSC = [ChainId.BSC]
-export const SUPPORT_FARMS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.ETHEREUM, ChainId.GOERLI]
+export const SUPPORT_ONLY_BSC = [ChainId.BSC, ChainId.POLYGOMIC]
+export const SUPPORT_FARMS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.ETHEREUM, ChainId.GOERLI, ChainId.POLYGOMIC]
 
-export const SUPPORT_ZAP = [ChainId.BSC, ChainId.BSC_TESTNET]
+export const SUPPORT_ZAP = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.POLYGOMIC]
