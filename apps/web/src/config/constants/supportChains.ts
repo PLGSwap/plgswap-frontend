163b1c330a6f453cd7947cb1bda52854d125b41e
git <eae966@gmail.com>
2023-03-17 12:04:04 +0330

    AllSupportedChains has been added.

--- apps/web/src/config/constants/supportChains.ts
+++ apps/web/src/config/constants/supportChains.ts
@@ -2,5 +2,5 @@ import { ChainId } from '@plgswap/sdk'
 
 export const SUPPORT_ONLY_BSC = [ChainId.BSC]
 export const SUPPORT_FARMS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.ETHEREUM, ChainId.GOERLI]
-
+export const AllSupportedChains = [ChainId.BSC, ChainId.ETHEREUM, ChainId.POLYGOMIC]
 export const SUPPORT_ZAP = [ChainId.BSC, ChainId.BSC_TESTNET]
